50397bf76d6e7b2088c478b02c37f84e97271d92
Georgia Beggs <Beggs-g4@ulster.ac.uk>
2025-11-10 20:36:31 +0000

    library example

---
+++ libraryexample/Library/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        //variable list
+
+        private string Title;
+        private string Author;
+        private string ISBN;
+
+        public string title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        public string author
+        {
+            get { return Author; }
+            set { Author = value; }
+        }
+        public string isbn
+        {
+            get { return ISBN; }
+            set { isbn = value; }
+        }
+
+        public void DisplayInfo() //display info 
+        {
+
+            Console.WriteLine($"Book Title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+
+        }
+
+
+        //Book Constructor 
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+
+        }
+
+
+    }
+}
---
+++ libraryexample/Library/Program.cs
@@ -0,0 +1,57 @@
+﻿using Library;
+using System.ComponentModel.DataAnnotations;
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        
+        // lists to store books and members
+
+        List<Book> books = new List<Book>();
+        List<Members> members = new List<Members>();
+
+        // create book object
+
+        books.Add(new Book("C# beginners", "Bill Gates", "12345678"));
+
+        //create book 1 object
+
+        books.Add(new Book("C# Methods and Classes", "Microsoft", "55667788"));
+
+
+
+        //Output book information to the console
+        Console.WriteLine("Current Books");
+        //book.DisplayInfo();
+        //book1.DisplayInfo();
+
+        //create member object 
+        members.Add(new Members(1, "John Smith", "1 High Street", 0790090090));
+        //create member object 1
+        members.Add(new Members(2, "Mary Jones", "102 Garden Road", 0790345666));
+
+        //member display info
+        Console.WriteLine("Current library members");
+        //member.DisplayInfo();
+        //member1.DisplayInfo();
+        foreach (Members member in members)
+        {
+            member.DisplayInfo();
+
+        }
+
+        foreach (Book book in books)
+        {
+            book.DisplayInfo();
+        }
+
+    }
+
+}
+
+
+
+
